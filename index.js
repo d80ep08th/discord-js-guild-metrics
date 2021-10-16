8e1fe9e565ab735a92a0ec5723fdba880ece482e
depth <depth@localhost.localdomain>
2021-10-17 05:00:51 +0530

    nodejs script that gets the member count of a server/guild

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+});
+
+client.login(token);
