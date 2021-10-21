627dcfc9e7f5a43f979e8297293327a32a1ac914
depth <depth@localhost.localdomain>
2021-10-21 14:28:09 +0530

    script with working test

--- index.js
+++ index.js
@@ -7,20 +7,18 @@ clientId = process.env.CLIENT_ID
 guildId = process.env.GUILD_ID
 token = process.env.DISCORD_TOKEN
 
-function getMembers(GUILD_ID){
+async function getMembers(GUILD_ID){
 	let count = 0;
-	const guild = client.guilds.cache.get(guildId);
-	count = guild.memberCount;
+	//client.login(token);
+	count = client.guilds.cache.get(guildId).memberCount;
 	console.log("member count==>" +count);
-	return count;
+	return client.guilds.cache.get(guildId).memberCount;
 }
 
 
 client.once('ready', () => {
-	console.log('Ready to count members!');
 	getMembers(guildId);
 });
-
-
 client.login(token);
 
+module.exports = { getMembers, client };
